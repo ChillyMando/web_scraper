7fe5076d99f658e86893afa23fc3e37486dda860
Colby Falk <colbyfalk95@gmail.com>
2023-08-13 20:03:00 -0500

    Renamed main.py to home_scrape.py to keep the in dir scrape file. Added new main.py that can scrape websites

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
+html_text = requests.get(
+    'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=Canada').text
 
-    soup = BeautifulSoup(content, 'lxml')
-    courses_html_tags = soup.find_all('h5')
-    for course in courses_html_tags:
-        print(course.text)
+
+soup = BeautifulSoup(html_text, 'lxml')
+job = soup.find('li', class_="clearfix job-bx wht-shd-bx")
+company_name = job.find(
+    'h3', class_='joblist-comp-name').text.replace(' ', '')
+print(company_name)
