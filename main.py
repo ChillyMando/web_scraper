6c7ccad710dab8ea20ca2cd1e3b93dca56aa0653
Colby Falk <colbyfalk95@gmail.com>
2023-08-15 18:24:12 -0500

    Filtered all job postings to recent. Added for loop to iterate all jobs.

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ html_text = requests.get(
 
 
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_="clearfix job-bx wht-shd-bx")
-company_name = job.find(
-    'h3', class_='joblist-comp-name').text.replace(' ', '')
-print(company_name)
+jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
+for job in jobs:
+    published_date = job.find(
+        'span', class_="sim-posted").text
+    if 'few' in published_date:
+        company_name = job.find(
+            'h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills = job.find(
+            'span', class_="srp-skills").text.replace(' ', '').replace(',', ', ')
+
+        print(f'''
+        Comapny Name: {company_name}
+        Required Skills: {skills}
+        Published Date: {published_date}
+        ''')
+        print('-----------------------------------')
